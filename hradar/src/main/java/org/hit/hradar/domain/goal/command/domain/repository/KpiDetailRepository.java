4a523da10f1291580d800e9286d919219eeb4621
yendlessse <yoohanse@naver.com>
2026-01-11 18:23:56 +0900

    fix: KpiDetailRepository, OkrKeyResultJpaRepository findById 메서드 수정

--- hradar/src/main/java/org/hit/hradar/domain/goal/command/domain/repository/KpiDetailRepository.java
+++ hradar/src/main/java/org/hit/hradar/domain/goal/command/domain/repository/KpiDetailRepository.java
@@ -5,6 +5,6 @@ import org.hit.hradar.domain.goal.command.domain.aggregate.KpiDetail;
 import java.util.Optional;
 
 public interface KpiDetailRepository {
-    KpiDetail save(KpiDetail kpi);
     Optional<KpiDetail> findById(Long id);
+    KpiDetail save(KpiDetail kpi);
 }
--- hradar/src/main/java/org/hit/hradar/domain/goal/command/domain/repository/OkrKeyResultRepository.java
+++ hradar/src/main/java/org/hit/hradar/domain/goal/command/domain/repository/OkrKeyResultRepository.java
@@ -5,6 +5,6 @@ import org.hit.hradar.domain.goal.command.domain.aggregate.OkrKeyResult;
 import java.util.Optional;
 
 public interface OkrKeyResultRepository {
-    OkrKeyResult save(OkrKeyResult kr);
     Optional<OkrKeyResult> findById(Long id);
+    OkrKeyResult save(OkrKeyResult kr);
 }
