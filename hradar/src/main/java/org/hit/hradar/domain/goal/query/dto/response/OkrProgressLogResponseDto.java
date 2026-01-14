1ee55b4e301fda4c37b5897ca2c6abab14448e5a
yendlessse <yoohanse@naver.com>
2026-01-14 17:33:10 +0900

    feat: OKR 상세조회

---
+++ hradar/src/main/java/org/hit/hradar/domain/goal/query/dto/response/OkrProgressLogResponseDto.java
@@ -0,0 +1,12 @@
+package org.hit.hradar.domain.goal.query.dto.response;
+
+import java.time.LocalDate;
+
+public class OkrProgressLogResponseDto {
+
+    private LocalDate logDate;
+
+    private Integer currentProgress;
+
+    private Integer progressRate;
+}
