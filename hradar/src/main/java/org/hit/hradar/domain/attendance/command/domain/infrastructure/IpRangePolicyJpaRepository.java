2d1aa851a276bc9e23b0b66e0bedb0ecd5c88bc1
sangdo19909-code <sangdo19909@gmail.com>
2026-01-16 15:29:10 +0900

    [feature] attendance IpPolicy
    postman test 완료(등록 수정 비활성/활성 삭제(소프트))

---
+++ hradar/src/main/java/org/hit/hradar/domain/attendance/command/domain/infrastructure/IpRangePolicyJpaRepository.java
@@ -0,0 +1,11 @@
+package org.hit.hradar.domain.attendance.command.domain.infrastructure;
+
+import org.hit.hradar.domain.attendance.command.domain.aggregate.IpRangePolicy;
+import org.hit.hradar.domain.attendance.command.domain.repository.IpRangePolicyRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IpRangePolicyJpaRepository extends JpaRepository<IpRangePolicy, Long>,
+    IpRangePolicyRepository {
+
+
+}
