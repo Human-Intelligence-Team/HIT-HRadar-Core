590a8c22f8103f05739013b59593298d243a68ed
sangdo19909-code <sangdo19909@gmail.com>
2026-01-24 12:31:58 +0900

    [feature] EMP_Leave
    휴가 결재 등록
    
    휴가 조회

--- hradar/src/main/java/org/hit/hradar/domain/leave/LeaveErrorCode.java
+++ hradar/src/main/java/org/hit/hradar/domain/leave/LeaveErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum LeaveErrorCode implements ErrorCode {
 
     LEAVE_OVERLAP("LEAVE_001", "이미 해당 기간에 휴가가 존재합니다.", HttpStatus.FOUND),
-    LEAVE_NOT_ENOUGH("LEAVE_002","잔여 연차가 부족합니다.", HttpStatus.TOO_EARLY);
+    LEAVE_NOT_ENOUGH("LEAVE_002","잔여 연차가 부족합니다.", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
--- hradar/src/main/java/org/hit/hradar/domain/leave/query/controller/LeaveQueryController.java
+++ hradar/src/main/java/org/hit/hradar/domain/leave/query/controller/LeaveQueryController.java
@@ -1,6 +1,18 @@
 package org.hit.hradar.domain.leave.query.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.angus.mail.iap.Response;
+import org.hit.hradar.domain.leave.query.dto.response.LeaveDetailDto;
+import org.hit.hradar.domain.leave.query.dto.response.LeaveGrantDto;
+import org.hit.hradar.domain.leave.query.dto.response.LeaveListDto;
+import org.hit.hradar.domain.leave.query.service.LeaveQueryService;
+import org.hit.hradar.global.aop.CurrentUser;
+import org.hit.hradar.global.dto.ApiResponse;
+import org.hit.hradar.global.dto.AuthUser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +20,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/leave")
 @RequiredArgsConstructor
 public class LeaveQueryController {
+
+  private final LeaveQueryService leaveQueryService;
+
+  //내 휴가 목록 조회
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<LeaveListDto>>> getMyLeaves(
+      @CurrentUser AuthUser authUser
+  ) {
+    return ResponseEntity.ok(
+        ApiResponse.success(
+            leaveQueryService.getMyLeaveList(authUser.employeeId())
+        )
+    );
+  }
+
+  //휴가 상세 조회
+  @GetMapping("/{leaveId}")
+  public ResponseEntity<ApiResponse<LeaveDetailDto>> getLeaveDetail (
+      @PathVariable Long leaveId
+  ) {
+    return ResponseEntity.ok(
+        ApiResponse.success(
+            leaveQueryService.getLeaveDetail(leaveId)
+        )
+    );
+  }
+
+  //연차 지급/잔여 조회
+  @GetMapping("/grant/{grantId}")
+  public ResponseEntity<ApiResponse<LeaveGrantDto>> getLeaveGrant(
+      @PathVariable Long grantId
+  ) {
+    return ResponseEntity.ok(
+        ApiResponse.success(
+            leaveQueryService.getLeaveGrant(grantId)
+        )
+    );
+  }
 
 
 
