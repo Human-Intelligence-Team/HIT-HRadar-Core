bfa725468170b28981119ff358cdc573aa07b528
sangdo19909-code <sangdo19909@gmail.com>
2026-01-24 19:21:47 +0900

    [feature] Approval
    임시저장, 상신 수정

--- hradar/src/main/java/org/hit/hradar/domain/approval/command/infrastructure/ApprovalReferenceJpaRepository.java
+++ hradar/src/main/java/org/hit/hradar/domain/approval/command/infrastructure/ApprovalReferenceJpaRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ApprovalReferenceJpaRepository
     extends JpaRepository<ApprovalReference, Long> {
 
+  void deleteByDocId(Long docId);
+
 }
