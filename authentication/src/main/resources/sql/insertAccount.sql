1c2f302f7c6874a9c009995fd485f28879fc448d
zkvn99 <zkvn1103@naver.com>
2026-02-02 16:22:23 +0900

    config : s3 file store, Presigned URL Loading

--- authentication/src/main/resources/sql/insertAccount.sql
+++ authentication/src/main/resources/sql/insertAccount.sql
@@ -1,4 +1,4 @@
-INSERT INTO hit.user_account (
+INSERT INTO user_account (
     com_id,
     company_code,
     employee_id,
@@ -7,15 +7,42 @@ INSERT INTO hit.user_account (
     password,
     name,
     role,
-    status
+    status,
+    is_deleted
 ) VALUES
-      (1, 'COMP001', 1001, 'user01', 'user01@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '김민수', 'user', 'ACTIVE'),
-      (2,'COMP002', 1002, 'user02', 'user02@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '이서연', 'user', 'ACTIVE'),
-      (2,'COMP003', 1003, 'user03', 'user03@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '박지훈', 'user', 'ACTIVE'),
-      (1,'COMP004', 1004, 'user04', 'user04@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '최유진', 'user', 'ACTIVE'),
-      (1,'COMP005', 1005, 'user05', 'user05@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '정우성', 'user', 'INACTIVE'),
-      (1,'COMP006', 2001, 'admin01', 'admin01@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '관리자1', 'admin', 'ACTIVE'),
-      (2,'COMP007', 2002, 'admin02', 'admin02@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '관리자2', 'admin', 'ACTIVE'),
-      (1,'COMP008', NULL, 'user06', 'user06@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '한지민', 'user', 'ACTIVE'),
-      (2,'COMP009', NULL, 'user07', 'user07@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '오세훈', 'user', 'ACTIVE'),
-      (1,'COMP010', NULL, 'user08', 'user08@test.com', '$2a$10$CgyohMOiILQ7Y612C5vvn.JLDb085wxSJA0aSnrdWhEtoV2KrCR3y', '윤하늘', 'user', 'ACTIVE');
+      (
+          1,
+          'COMP001',
+          1001,
+          'user01',
+          'user01@test.com',
+          '$2b$10$ocLiue.qK3S7vygj8IlCyuEGW6JQb0l8dAQ63BAbx795DY9N43NP.',
+          '테스트유저1',
+          'USER',
+          'ACTIVE',
+          'N'
+      ),
+      (
+          1,
+          'COMP001',
+          1002,
+          'user02',
+          'user02@test.com',
+          '$2b$10$ocLiue.qK3S7vygj8IlCyuEGW6JQb0l8dAQ63BAbx795DY9N43NP.',
+          '테스트유저2',
+          'USER',
+          'ACTIVE',
+          'N'
+      ),
+      (
+          1,
+          'COMP001',
+          1003,
+          'admin01',
+          'admin01@test.com',
+          '$2b$10$ocLiue.qK3S7vygj8IlCyuEGW6JQb0l8dAQ63BAbx795DY9N43NP.',
+          '관리자',
+          'ADMIN',
+          'ACTIVE',
+          'N'
+      );
